35ce52f3ad2d0c7506497360714f1cb66758cb42
Arif Muharam <arifmuharam20@gmail.com>
2024-12-27 18:07:04 +0700

    feat/main/buat interaksi navigasi dengan javascript

--- src/main.js
+++ src/main.js
@@ -1,1 +1,39 @@
 import './scss/style.scss';
+
+const body = document.querySelector('body');
+const btnOpen = document.querySelector('#btnOpen');
+const btnClose = document.querySelector('#btnClose');
+const media = window.matchMedia('(width < 69.375em)');
+const navContent = document.querySelector('.nav__content');
+const navOverlay = document.querySelector('.nav__overlay');
+
+function openMobileNavigation() {
+  btnOpen.setAttribute('aria-expanded', 'true');
+  body.classList.add('noscroll');
+}
+
+function closeMobileNavigation() {
+  btnOpen.setAttribute('aria-expanded', 'false');
+  body.classList.remove('noscroll');
+}
+
+function setupNav(e) {
+  if (e.matches) {
+    // Mobile
+    setTimeout(() => {
+      navContent.style.display = 'block';
+      navOverlay.style.display = 'block';
+    }, 500);
+  } else {
+    navContent.style.display = '';
+  }
+}
+
+setupNav(media);
+
+btnOpen.addEventListener('click', openMobileNavigation);
+btnClose.addEventListener('click', closeMobileNavigation);
+
+media.addEventListener('change', (e) => {
+  setupNav(e);
+});
