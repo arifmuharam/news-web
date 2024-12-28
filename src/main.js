ea50003236ce3d6137fa44a0e4d93cb4d43c7d3d
Arif Muharam <arifmuharam20@gmail.com>
2024-12-28 14:01:07 +0700

    feat/main/menambahkan fitur aksesibilitas

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import './scss/style.scss';
 
 const body = document.querySelector('body');
+const main = document.querySelector('main');
 const btnOpen = document.querySelector('#btnOpen');
 const btnClose = document.querySelector('#btnClose');
 const media = window.matchMedia('(width < 69.375em)');
@@ -10,22 +11,33 @@ const navOverlay = document.querySelector('.nav__overlay');
 function openMobileNavigation() {
   btnOpen.setAttribute('aria-expanded', 'true');
   body.classList.add('noscroll');
+  navContent.removeAttribute('inert');
+  main.setAttribute('inert', '');
+  btnClose.focus();
 }
 
 function closeMobileNavigation() {
   btnOpen.setAttribute('aria-expanded', 'false');
   body.classList.remove('noscroll');
+  navContent.setAttribute('inert', '');
+  main.removeAttribute('inert');
+  btnOpen.focus();
 }
 
 function setupNav(e) {
   if (e.matches) {
     // Mobile
+    navContent.setAttribute('inert', ''); // Atur Aksesibilitas (Screen Reader) Saat Tab Tidak ke Menu nav__content
+
     setTimeout(() => {
       navContent.style.display = 'block';
       navOverlay.style.display = 'block';
     }, 500);
   } else {
+    // Desktop
     navContent.style.display = '';
+    navContent.removeAttribute('inert');
+    main.removeAttribute('inert');
   }
 }
 
@@ -36,4 +48,11 @@ btnClose.addEventListener('click', closeMobileNavigation);
 
 media.addEventListener('change', (e) => {
   setupNav(e);
+});
+
+// Atur Aksesibilitas (Screen Reader)
+document.addEventListener('keyup', (e) => {
+  if (e.key == 'Tab') {
+    console.log(document.activeElement);
+  }
 });
